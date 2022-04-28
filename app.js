5e0eeade15cd9cc9ae4319e336facde4bafc49a3
Sachin-Sharma-1 <sachinsharma26112000@gmail.com>
2022-04-28 23:02:34 +0530

    updated for heroku

--- app.js
+++ app.js
@@ -48,6 +48,6 @@ app.post("/weather", function(req, res){
   res.redirect("/");
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server started on port 3000");
 });
